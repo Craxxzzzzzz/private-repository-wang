38ba4b26d8cf811f001daac276f12fe59b844824
Craxxzzzzzz <patrickwang132007@gmail.com>
2024-10-16 15:54:56 +0200

    ivt16.10

---
+++ spojovy_seznamy/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Node uzlik = new Node(8);
+        }
+    }
+    class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; }
+        public Node Next { get; set; }
+    }
+class LinkedList
+{
+    public Node Head { get; set; }
+    public void Add(int value)
+    {
+        if (Head == null)
+            Head = new Node(value);
+        else
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Head;
+            Head = newNode; 
+
+        }
+    }
+    public bool Find(int value)
+    {
+        if(Head == null)
+            return false;
+        Node node = Head;
+        while (node != null)
+        {
+            if(node.Value == value)
+                return true;
+            node = node.Next;
+        }
+        return false;
+    }
+
+
+}
