8e75291f3e3b1b79dd1824259ec56b40d1c7906e
Craxxzzzzzz <patrickwang132007@gmail.com>
2024-10-23 15:56:06 +0200

    implementace spojových seznamů

--- spojovy_seznamy/ConsoleApp1/Program.cs
+++ spojovy_seznamy/ConsoleApp1/Program.cs
@@ -20,7 +20,12 @@ class Program
         public Node Next { get; set; }
     }
 class LinkedList
+
 {
+    public LinkedList()
+    {
+
+    }
     public Node Head { get; set; }
     public void Add(int value)
     {
@@ -30,23 +35,78 @@ class LinkedList
         {
             Node newNode = new Node(value);
             newNode.Next = Head;
-            Head = newNode; 
+            Head = newNode;
 
         }
     }
     public bool Find(int value)
     {
-        if(Head == null)
+        if (Head == null)
             return false;
         Node node = Head;
         while (node != null)
         {
-            if(node.Value == value)
+            if (node.Value == value)
                 return true;
             node = node.Next;
         }
         return false;
+
     }
+    public void PrintLinkedList()
+    {
+        Node prvek = Head;
+        while (prvek != null) 
+        {
+            Console.Write(prvek.Value);
+            Console.Write(", ");
+            prvek = prvek.Next;
+        }
+
+    }
+    public void SortLinkedList()
+    {
+        LinkedList newList = new LinkedList();
+        newList.Head = new Node (Head.Value);
+        if (Head != null)
+        {
+            Node prvek = Head.Next;
+            while (prvek != null)
+            {
+                newList.InsertSorted(prvek.Value);
+                prvek = prvek.Next;
+            }
+        }       
+    }
+    public void InsertSorted(int value)
+    {
+        if (Head == null)
+            Head = new Node(value);
+        else
+        {
+            Node prvek = Head;
+            while (prvek != null)
+            {
+                if (prvek.Value < Value)
+                    if (prvek.Next != null)
+                        prvek = prvek.Next;
+                    else
+                        break;
+                else
+                    break;
+
+            }
+            if (prvek != null)
+            {
+                Node newNode = new Node(value);
+                newNode.Next = prvek.Head;
+                prvek.Next = newNode;
+            }
+
+        }
+
+    }
+        
 
 
 }
