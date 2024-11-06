5610fc7f9980f86e216a74f0141087bd4bedd510
Craxxzzzzzz <patrickwang132007@gmail.com>
2024-11-06 13:14:53 +0100

    Dokončení destruktivního rozdělení

--- spojovy_seznamy/ConsoleApp1/Program.cs
+++ spojovy_seznamy/ConsoleApp1/Program.cs
@@ -7,20 +7,44 @@ class Program
     {
         static void Main(string[] args)
         {
-            LinkedList list = new LinkedList();
-            list.Add(3);
-            list.Add(6);
-            list.Add(4);
-            list.Add(5);
-            list.Add(7);
+            LinkedList list1 = new LinkedList();
+            list1.Add(3);
+            list1.Add(6);
+            list1.Add(4);
+            list1.Add(5);
+            list1.Add(7);
 
-        Console.WriteLine("původní LL");
-        list.PrintLinkedList();
-        list.SortLinkedList();
-        Console.WriteLine("\nseřazený LL");
-        list.PrintLinkedList();
+            LinkedList list2 = new LinkedList();
+            list2.Add(3);
+            list2.Add(6);
+            list2.Add(4);
+            list2.Add(5);
+            list2.Add(7);
+
+
+
+        Console.WriteLine("původní LL1");
+        list1.PrintLinkedList();
+
+        Console.WriteLine("původní LL2");
+        list2.PrintLinkedList();
+        
+        list1.SortLinkedList();
+        list2.SortLinkedList();
+        
+        Console.WriteLine("\nseřazený LL1");
+        list1.PrintLinkedList();
+        
+        Console.WriteLine("\nseřazený LL2");
+        list2.PrintLinkedList();
+
+        LinkedList result = LinkedList.DestruktivniRozdeleni(list1, list2);
+        Console.WriteLine("\nDestruktivní rozdělení");
+        result.PrintLinkedList();
+
         }
-    }
+    
+
     class Node
     {
         public Node(int value)
@@ -46,6 +70,45 @@ class LinkedList
 
         }
     }
+    public static LinkedList DestruktivniRozdeleni (LinkedList list1, LinkedList list2)
+        {
+            LinkedList result = new LinkedList();
+            HashSet<int> set = new HashSet<int>(); // našel jsem na internetu, vůbec nevím co to dělá
+            
+            Node prvek1 = list1.Head;
+            while (prvek1 != null)
+            {
+                set.Add(prvek1.Value);
+                prvek1 = prvek1.Next;
+            }
+
+            Node prvek2 = list2.Head;
+            Node resultTail = null;
+
+            while (prvek2 != null)
+            {
+                if (set.Contains(prvek2.Value))
+                {
+                    set.Remove(prvek2.Value);
+
+                    Node newNode = new Node (prvek2.Value);
+
+                    if (result.Head == null)
+                    {
+                        result.Head = newNode;
+                        resultTail = newNode; // nový pointer co ukazuje nový list, můžeme přidávat nový node na konci výsledku
+                    }
+
+                    else
+                    {
+                        resultTail.Next = newNode;
+                        resultTail = resultTail.Next;
+                    }
+                }
+                prvek2 = prvek2.Next;
+            }
+            return result;
+        }
     public bool Find(int value)
     {
         if (Head == null)
@@ -105,7 +168,7 @@ class LinkedList
             Node prvek = Head;
             while (prvek != null && prvek.Next.Value < newNode.Value)
             {
-                prvek = (Node)prvek.Next;
+                prvek = prvek.Next;
 
             }
             newNode.Next = prvek.Next;
@@ -114,7 +177,4 @@ class LinkedList
         }
 
     }
-        
-
-
 }
