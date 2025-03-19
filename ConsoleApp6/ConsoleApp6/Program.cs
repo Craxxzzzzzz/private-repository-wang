ef27d579b1f3d886ca4da1cd0583f67de9ef3f3b
Craxxzzzzzz <patrickwang132007@gmail.com>
2025-03-19 17:48:27 +0100

    zaklad

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+
+namespace connect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static bool Check(int numPlayers, int [,] field, int winReq, int[] curPosition)
+        {
+            return column(numPlayers, field, winReq, curPosition) || row(numPlayers, field, winReq, curPosition) || diagonal(numPlayers, field, winReq, curPosition);
+        }
+
+        static bool column(int numPlayer, int[,] field, int winReq, int[] curPosition)
+        {
+            int positionColumn = field.GetLength(curPosition[0]);
+            int positionRow = field.GetLength(curPosition[1]);
+
+            static int FirstRowUnder(int positionRow, int positionColumn)
+            {
+                int value = field[positionColumn, positionRow];
+
+                if 
+
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        static bool row(int numPlayer, int[,] field, int winReq, int[] curPosition)
+        {
+
+        }
+
+        static bool diagonal(int numPlayer, int[,] field, int winReq, int[] curPosition)
+    }
+}
